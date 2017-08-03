86f6d21d5a274035960103937a3707cfbeedae2d
kikuchy <kikuchy.h@gmail.com>
2017-08-03 14:18:46 +0900

    画像が出るようにした

--- app/src/main/java/net/kikuchy/loaddatas/StargazerListAdapter.kt
+++ app/src/main/java/net/kikuchy/loaddatas/StargazerListAdapter.kt
@@ -2,11 +2,14 @@ package net.kikuchy.loaddatas
 
 import android.arch.lifecycle.LiveData
 import android.content.Context
+import android.net.Uri
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Picasso
 import net.kikuchy.loaddatascore.stargazer.Stargazer
 
 /**
@@ -14,6 +17,7 @@ import net.kikuchy.loaddatascore.stargazer.Stargazer
  */
 class StargazerListAdapter(context: Context, val stargazers: MutableList<Stargazer>): RecyclerView.Adapter<StargazerListAdapter.ViewHolder>() {
     private val inflater = LayoutInflater.from(context)
+    private val imageLoader = Picasso.with(context)
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): StargazerListAdapter.ViewHolder {
         return ViewHolder(inflater.inflate(R.layout.list_item, parent, false))
@@ -24,11 +28,14 @@ class StargazerListAdapter(context: Context, val stargazers: MutableList<Stargaz
     }
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        holder?.name?.text = stargazers[position].loginName
+        if (holder == null) return
+
+        holder.name.text = stargazers[position].loginName
+        imageLoader.load(stargazers[position].avatarUrl.toASCIIString()).into(holder.avatar)
     }
 
     class ViewHolder(item : View) : RecyclerView.ViewHolder(item) {
         val name: TextView = item.findViewById(R.id.name)
-
+        val avatar: ImageView = item.findViewById(R.id.avatar)
     }
 }
